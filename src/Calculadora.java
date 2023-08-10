d8915aba4734059f007ea406b471c6a7068fac0e
Daniel Sanchez <daniel.sanchez@apriorins.com>
2023-08-10 00:23:38 -0500

    Primera versión, incluye la suma de dos números enteros.

---
+++ src/Calculadora.java
@@ -0,0 +1,11 @@
+public class Calculadora {
+    public static void main(String[] args) {
+        // Definición de 2 variables enteras:
+        int numero1 = 11;
+        int numero2 = 5;
+
+        // Operación suma:
+        int suma = numero1 + numero2;
+        System.out.println("La suma es " + suma);
+    }
+}
