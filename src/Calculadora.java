e19c24387aa46c9841cf48b926e7e318cd4df922
Daniel Sanchez <daniel.sanchez@apriorins.com>
2023-08-10 01:14:15 -0500

    La anterior solución tenía el siguiente inconveniente:
    En el peor de los casos (La división) era necesario ejecutar las líneas 14, 19 y 24 (Comparación de las operaciones de suma, resta y multiplicación respectivamente).
    
    Razón por la cual se cambió la validación con la estructura de control if-else por la estructura de control switch case.

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -11,27 +11,31 @@ public class Calculadora {
         System.out.println("Ingrese la operación (+, -, *, /)");
         char operacion = lector.next().charAt(0);
 
-        if (operacion == '+') {
-            // Operación suma:
-            int suma = numero1 + numero2;
-            System.out.println("La suma es " + suma);
-        }
-        else if (operacion == '-') {
-            // Operación resta:
-            int resta = numero1 - numero2;
-            System.out.println("La resta es " + resta);
-        }
-        else if (operacion == '*') {
-            // Operación multiplicación:
-            int multiplicacion = numero1 * numero2;
-            System.out.println("La multiplicación es " + multiplicacion);
-        }
-        else if (operacion == '/') {
-            // Operación división:
-            /* Es necesario que uno de los números que intervienen en la división sea double
-             para que el resultado decimal sea aproximado correctamente. **/
-            double division = (double) numero1 / numero2;
-            System.out.println("La división es " + division);
+        switch (operacion) {
+            case '+':
+                // Operación suma:
+                int suma = numero1 + numero2;
+                System.out.println("La suma es " + suma);
+                break;
+            case '-':
+                // Operación resta:
+                int resta = numero1 - numero2;
+                System.out.println("La resta es " + resta);
+                break;
+            case '*':
+                // Operación multiplicación:
+                int multiplicacion = numero1 * numero2;
+                System.out.println("La multiplicación es " + multiplicacion);
+                break;
+            case '/':
+                // Operación división:
+                /* Es necesario que uno de los números que intervienen en la división sea double
+                para que el resultado decimal sea aproximado correctamente. **/
+                double division = (double) numero1 / numero2;
+                System.out.println("La división es " + division);
+                break;
+            default:
+                System.out.println("Operación inválida");
         }
     }
 }
