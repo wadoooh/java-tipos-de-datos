37246d095d09d36d521a7566835031e046a56421
Daniel Sanchez <daniel.sanchez@apriorins.com>
2023-08-10 01:00:20 -0500

    La anterior solución tenía el siguiente inconveniente:
    Si se ejecuta la operación de la Suma, se ejecutan las siguientes líneas:
    1) Línea 14 para la comparación con el operador suma.
    2) Líneas 16 y 17 para la suma con tal.
    3) Líneas 19, 24 y 29. Para las comparaciones con el operador resta, multiplicación y división respectivamente.
    
    En este caso el paso 3) para la operación de la suma es innecesario, por ende se adicionan condicionales if-else con el fin de evitar la ejecución de dichas líneas.

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -16,17 +16,17 @@ public class Calculadora {
             int suma = numero1 + numero2;
             System.out.println("La suma es " + suma);
         }
-        if (operacion == '-') {
+        else if (operacion == '-') {
             // Operación resta:
             int resta = numero1 - numero2;
             System.out.println("La resta es " + resta);
         }
-        if (operacion == '*') {
+        else if (operacion == '*') {
             // Operación multiplicación:
             int multiplicacion = numero1 * numero2;
             System.out.println("La multiplicación es " + multiplicacion);
         }
-        if (operacion == '/') {
+        else if (operacion == '/') {
             // Operación división:
             /* Es necesario que uno de los números que intervienen en la división sea double
              para que el resultado decimal sea aproximado correctamente. **/
