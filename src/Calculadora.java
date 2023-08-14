516a38b80c21facdd55504fb90cc28e7a9be9cd8
Daniel Sanchez <daniel.sanchez@apriorins.com>
2023-08-14 15:04:34 -0500

    Se revierten los cambios para que no utilice en "enhance switch".

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -12,29 +12,30 @@ public class Calculadora {
         char operacion = lector.next().charAt(0);
 
         switch (operacion) {
-            case '+' -> {
+            case '+' :
                 // Operación suma:
                 int suma = numero1 + numero2;
                 System.out.println("La suma es " + suma);
-            }
-            case '-' -> {
+                break;
+            case '-' :
                 // Operación resta:
                 int resta = numero1 - numero2;
                 System.out.println("La resta es " + resta);
-            }
-            case '*' -> {
+                break;
+            case '*' :
                 // Operación multiplicación:
                 int multiplicacion = numero1 * numero2;
                 System.out.println("La multiplicación es " + multiplicacion);
-            }
-            case '/' -> {
+                break;
+            case '/' :
                 // Operación división:
                 /* Es necesario que uno de los números que intervienen en la división sea double
                 para que el resultado decimal sea aproximado correctamente. **/
                 double division = (double) numero1 / numero2;
                 System.out.println("La división es " + division);
-            }
-            default -> System.out.println("Operación inválida");
+                break;
+            default:
+                System.out.println("Operación inválida");
         }
     }
 }
