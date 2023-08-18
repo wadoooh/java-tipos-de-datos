fab0ffb47c88cfd5a83f631a11fe7fd591ad5641
wadoooh <juansebastiansuarez1001icl@gmail.com>
2023-08-18 12:40:23 -0500

    potencia modulo

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -8,34 +8,41 @@ public class Calculadora {
         int numero1 = lector.nextInt();
         System.out.println("Ingrese el número 2");
         int numero2 = lector.nextInt();
-        System.out.println("Ingrese la operación (+, -, *, /)");
+        System.out.println("Ingrese la operación (+, -, *, /,^,%)");
         char operacion = lector.next().charAt(0);
 
         switch (operacion) {
-            case '+' :
+            case '+' -> {
                 // Operación suma:
                 int suma = numero1 + numero2;
                 System.out.println("La suma es " + suma);
-                break;
-            case '-' :
+            }
+            case '-' -> {
                 // Operación resta:
                 int resta = numero1 - numero2;
                 System.out.println("La resta es " + resta);
-                break;
-            case '*' :
+            }
+            case '*' -> {
                 // Operación multiplicación:
                 int multiplicacion = numero1 * numero2;
                 System.out.println("La multiplicación es " + multiplicacion);
-                break;
-            case '/' :
+            }
+            case '/' -> {
                 // Operación división:
                 /* Es necesario que uno de los números que intervienen en la división sea double
                 para que el resultado decimal sea aproximado correctamente. **/
                 double division = (double) numero1 / numero2;
                 System.out.println("La división es " + division);
-                break;
-            default:
-                System.out.println("Operación inválida");
+            }
+            case '^' -> {
+                double potencia = Math.pow(numero1, numero2);
+                System.out.println("La potencia es" + potencia);
+            }
+            case '%' -> {
+                int modulo = numero1 % numero2;
+                System.out.println("El porcentaje es" + modulo);
+            }
+            default -> System.out.println("Operación inválida");
         }
     }
 }
